876a4a65c15073e207954d4ee4299eb332ae74d9
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-03 09:37:25 +0300

    add the jsonwebtoken

---
+++ server.js
@@ -0,0 +1,29 @@
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const PORT = process.env.PORT ? process.env.PORT : "3000"
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}. 🥭`)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+// Routes go here
+
+app.listen(PORT, () => {
+  console.log(`The express app is ready on port ${PORT}! 😀`)
+})
